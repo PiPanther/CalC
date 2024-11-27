ed097267972bc15efb169d4ad52f4e2a4b034bee
A   S   H   I   S   H <96686068+PiPanther@users.noreply.github.com>
2024-11-27 17:25:11 +0530

    Additions : FrontEnd

--- calc-frontend/src/screens/home/index.tsx
+++ calc-frontend/src/screens/home/index.tsx
@@ -1,7 +1,35 @@
-import { useEffect, useRef, useState } from "react";
+import { ColorSwatch, Group } from '@mantine/core';
+import { Button } from '@/components/ui/button';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import {SWATCHES} from '@/constants';
+
+// interface Response {
+//     expr : string;
+//     result : string;
+//     assign : boolean;
+// }
+
+// interface GeneratedResult {
+//     expression : string;
+//     answer : string;
+// }
 export default function Home() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isDrawing, setIsDrawing] = useState(false);
+    const [reset, setReset] = useState(false);
+    const [color, setColor] = useState('rgb(255,255,255)');
+    // const [result, setResult] = useState<GeneratedResult>(); 
+    // const [dictofVariables, setDictofVariables] = useState({});
+
+
+
+    useEffect(() => {
+        if(reset) {
+            resetCanvas();
+            setReset(false);
+        };
+    }, [reset]);
     useEffect (() => {
         const canvas = canvasRef.current;
 
@@ -16,6 +44,23 @@ export default function Home() {
             }
         }
     }, []);
+
+    const sendData = async () => {
+        const canvas = canvasRef.current;
+
+        if(canvas) {
+            const response =  await axios({
+                method : 'post',
+                url : `${import.meta.env.VITE_API_URL}/calculate`,
+                data : {
+                    image : canvas.toDataURL('image/png'),
+                    dictofVariables : dictofVariables,
+                }
+            });
+            const result = await response.data;
+            console.log(result);
+        }
+    }
     const startDrawing = (e : React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
         if(canvas) {
@@ -29,6 +74,15 @@ export default function Home() {
         }
     };
 
+    const resetCanvas =  () => {
+        const canvas = canvasRef.current;
+        if(canvas) {
+            const ctx = canvas.getContext('2d');
+            if(ctx) {
+                ctx.clearRect(0,0,canvas.width, canvas.height);
+            }
+        }
+    }
     const stopDrawing = () => {
         setIsDrawing(false);
     };
@@ -41,7 +95,7 @@ export default function Home() {
         if(canvas) {
             const ctx = canvas.getContext('2d');
             if(ctx) {
-                ctx.strokeStyle = 'white';
+                ctx.strokeStyle = color;
                 ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
                 ctx.stroke();
             }
@@ -49,7 +103,37 @@ export default function Home() {
     }
 
     return (
-        <canvas ref = {canvasRef}
+        <>
+        <div className="grid grid-cols-3 gap-2">
+            <Button onClick={() => setReset(true)} 
+            className="z-20 bg-black text-white"
+            variant='default'
+            color="black"
+            >
+                Reset
+            </Button>
+            
+            <Group
+            className="z-20"
+            >
+                {SWATCHES.map((swatchColor : string) => (
+                    <ColorSwatch
+                    key={swatchColor}
+                    color={swatchColor}
+                    onClick={() => setColor(swatchColor)} 
+                    />
+                ))} 
+            </Group>
+            <Button onClick={sendData} 
+            className="z-20 bg-black text-white"
+            variant='default'
+            color="black"
+            >
+                Calculate
+            </Button>
+        </div>
+        <canvas 
+        ref = {canvasRef}
         id="canvas"
         className="absolute top-0 left-0 w-full h-full"
         onMouseDown={startDrawing}
@@ -57,5 +141,6 @@ export default function Home() {
         onMouseUp={stopDrawing}
         onMouseMove={draw}
         />
+        </>
     );
 }
